e53ebfb58475a58497b87af76939bc2231ee9b36
Ray <rayhh666@foxmail.com>
2025-07-25 11:20:11 +0800

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/tlstool/TlsToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.tlstool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlsToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlsToolApplication.class, args);
+    }
+
+}
